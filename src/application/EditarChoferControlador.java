24af9e8ca1a9906e1609d43368931fa3b6e894d1
AlanSZK <54686091+AlanSZK@users.noreply.github.com>
2021-11-10 19:44:30 -0300

    Actualizacion de funcionalidad vista clientes

--- src/application/EditarChoferControlador.java
+++ src/application/EditarChoferControlador.java
@@ -1,10 +1,22 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class EditarChoferControlador {
+	
+	@FXML public TextField idUsuario=new TextField();
+	@FXML private TextField nombreUsuario=new TextField();
+	@FXML private TextField rutUsuario=new TextField();
+	@FXML private TextField contrasenaUsuario=new TextField();
+	@FXML private TextField nombreChofer=new TextField();
+	@FXML private TextField telefonoChofer=new TextField();
+	
+	
+	
 	public void confirmar (ActionEvent e)
 	{
 		
@@ -16,4 +28,17 @@ public class EditarChoferControlador {
 		stage.close();
 		
 	}
+
+	public void inicializarVariables(int id, String usr, String rut, String con, String nom, String tel)
+	{
+		idUsuario.setText(String.valueOf(id));
+		nombreUsuario.setText(usr);
+		rutUsuario.setText(rut);
+		contrasenaUsuario.setText(con);
+		nombreChofer.setText(nom);
+		telefonoChofer.setText(tel);
+		
+	}
+	
+
 }
